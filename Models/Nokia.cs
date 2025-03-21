6dfc6c8afc2fc4626026b077e4cfe87a5584bd86
laurofraga <fragalauro12@gmail.com>
2025-03-21 15:31:43 -0300

    envio da solucao

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,23 @@
+using System.Runtime.CompilerServices;
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+            Numero = numero;
+    
+        }
+
+        
+        
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine( $"Instalando {nomeApp}");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+        {
+        // TODO: Realizar os testes com as classes Nokia e Iphone
+        Console.WriteLine("Testando Nokia");
+        Nokia nokia = new Nokia(numero: "123456789", modelo: "Nokia 3310", imei: "123456789", memoria: 16);
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+
+        Console.WriteLine("Testando Iphone");
+        Iphone iphone = new Iphone(numero: "987654321", modelo: "Iphone 12", imei: "987654321", memoria: 128);
+        iphone.Ligar();
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
